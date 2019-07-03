0548c7a8894164d4e6fdb58e5dc544dc145e95e8
James Liu <james@pop-os.localdomain>
2019-07-02 17:24:36 -0700

    eat mushroom done

--- 04-03-eat-mushroom.clj
+++ 04-03-eat-mushroom.clj
@@ -2,3 +2,9 @@
                          (if (< height 3)
                            :grow
                            :shrink)))
+
+(defmethod eat-mushroom :grow [_]
+  "Eat the right side to grow.")
+
+(defmethod eat-mushroom :shrink [_]
+  "Eat the left side to shrink.")
